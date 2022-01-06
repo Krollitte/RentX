9a6bebbf2cbc37165f5af0ccacd194dc7e100ea0
Krollitte <rsantoslfap@gmail.com>
2022-01-06 17:10:47 -0300

    Interface de agendamento

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { StatusBar, View } from "react-native";
+import { useTheme } from "styled-components";
+
+import { BackButton } from "../../components/BackButton";
+
+import ArrowSvg from "../../assets/arrow.svg";
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer,
+} from "./styles";
+
+import { Button } from "../../components/Button";
+
+export function Scheduling() {
+  const theme = useTheme();
+  return (
+    <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
+
+        <BackButton color={theme.colors.shape} />
+
+        <Title>
+          Escolha uma data{"\n"}de início e {"\n"}fim do aluguel{" "}
+        </Title>
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected={false}>18/06/2022</DateValue>
+          </DateInfo>
+          <ArrowSvg />
+          <DateInfo>
+            <DateTitle>ATÉ</DateTitle>
+            <DateValue selected={false}>18/06/2022</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+      <Content></Content>
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container>
+  );
+}
