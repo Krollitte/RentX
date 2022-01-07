6706bba9c0b219c736b0a1d7532f255561a7bafc
Krollitte <rsantoslfap@gmail.com>
2022-01-07 11:51:24 -0300

    Componente de Calendário

---
+++ src/components/Calendar/index.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Feather } from "@expo/vector-icons";
+import { useTheme } from "styled-components";
+import {
+  Calendar as CustomCalendar,
+  LocaleConfig,
+} from "react-native-calendars";
+
+LocaleConfig.locales["pt-br"] = {
+  monthNames: [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ],
+  monthNamesShort: [
+    "Jan",
+    "Fev",
+    "Mar",
+    "Abr",
+    "Mai",
+    "Jun",
+    "Jul",
+    "Ago",
+    "Set",
+    "Out",
+    "Nov",
+    "Dez",
+  ],
+  dayNames: ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"],
+  dayNamesShort: ["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SAB"],
+};
+
+LocaleConfig.defaultLocale = "pt-br";
+
+export function Calendar() {
+  const theme = useTheme();
+  return (
+    <CustomCalendar
+      renderArrow={(direction) => (
+        <Feather
+          size={24}
+          color={theme.colors.text}
+          name={direction == "left" ? "chevron-left" : "chevron-right"}
+        />
+      )}
+      headerStyle={{
+        backgroundColor: theme.colors.background_secondary,
+        borderBottomWidth: 0.5,
+        borderBottomColor: theme.colors.text_details,
+        paddingBottom: 10,
+        marginBottom: 10,
+      }}
+      theme={{
+        textDayFontFamily: theme.fonts.primary_400,
+        textDayHeaderFontFamily: theme.fonts.primary_500,
+        textMonthFontFamily: theme.fonts.secondary_600,
+        textDayHeaderFontSize: 10,
+        textMonthFontSize: 20,
+        monthTextColor: theme.colors.title,
+        arrowStyle: {
+          marginHorizontal: -15,
+        },
+      }}
+      firstDay={1}
+      minDate={String(new Date())}
+    />
+  );
+}
