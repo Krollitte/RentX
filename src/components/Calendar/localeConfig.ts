4e9992ffa645cf08f372296f5e9511d9eeef4cec
RafaelSantos <rsantoslfap@gmail.com>
2022-01-24 22:46:19 -0300

    Intervalos no calendário

---
+++ src/components/Calendar/localeConfig.ts
@@ -0,0 +1,35 @@
+const ptBR = {
+  monthNames: [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ],
+  monthNamesShort: [
+    "Jan",
+    "Fev",
+    "Mar",
+    "Abr",
+    "Mai",
+    "Jun",
+    "Jul",
+    "Ago",
+    "Set",
+    "Out",
+    "Nov",
+    "Dez",
+  ],
+  dayNames: ["Segunda", "Terça", "Quarta", "Quinta", "Sexta"],
+  dayNamesShort: ["DOM", "SEG", "TER", "QUA", "QUI", "SEX", "SAB"],
+  today: "Hoje",
+};
+
+export { ptBR };
