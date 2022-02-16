b03093ef287e5837967698ce7c46fb46d3e04db9
RafaelSantos <rsantoslfap@gmail.com>
2022-02-16 00:13:51 -0300

    Listando agendamento do usuario

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { CarDTO } from "../../dtos/CarDTO";
+import { api } from "../../services/api";
 
 import { Container } from "./styles";
 
 export function MyCars() {
+  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchCars() {
+      try {
+        const response = await api.get("/schedules_bycars/?user_id=1");
+        setCars(response.data);
+      } catch (err) {
+        console.error("Não foi possível carregar", err);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchCars();
+  }, []);
   return <Container></Container>;
 }
