c95af9dfe6ddf3d92bfbaae951c681b44e93baf3
RafaelSantos <rsantoslfap@gmail.com>
2022-02-16 21:47:59 -0300

    transições

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,21 +1,34 @@
 import React from "react";
-import { Button, StyleSheet } from "react-native";
+import { Button, StyleSheet, Dimensions } from "react-native";
+
 import Animated, {
   useSharedValue,
   useAnimatedStyle,
+  withTiming,
+  Easing,
 } from "react-native-reanimated";
+
+const WIDTH = Dimensions.get("window").width;
+
 import { Container } from "./styles";
 
 export function Splash() {
   const animation = useSharedValue(0);
   const animatedStyles = useAnimatedStyle(() => {
     return {
-      transform: [{ translateX: animation.value }],
+      transform: [
+        {
+          translateX: withTiming(animation.value, {
+            duration: 3000,
+            easing: Easing.bezier(0.01, 1.07, 1, -0.28),
+          }),
+        },
+      ],
     };
   });
 
   function handleAnimationPosition() {
-    animation.value = Math.random() * 500;
+    animation.value = Math.random() * (WIDTH - 100);
   }
 
   return (
