3e93cd37b59294bce5091a0e633e663c3f2a2000
RafaelSantos <rsantoslfap@gmail.com>
2022-02-27 23:04:59 -0300

    Tela de login

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -8,12 +8,14 @@ import { SchedulingComplete } from "../screens/SchedulingComplete";
 import { SchedulingDetails } from "../screens/SchedulingDetails";
 import { MyCars } from "../screens/MyCars";
 import { Splash } from "../screens/Splash";
+import { SignIn } from "../screens/SignIn";
 
 const { Navigator, Screen } = createStackNavigator();
 
 export function StackRoutes() {
   return (
-    <Navigator screenOptions={{ headerShown: false }} initialRouteName="Splash">
+    <Navigator screenOptions={{ headerShown: false }} initialRouteName="SignIn">
+      <Screen name="SignIn" component={SignIn} />
       <Screen name="Splash" component={Splash} />
       <Screen
         name="Home"
