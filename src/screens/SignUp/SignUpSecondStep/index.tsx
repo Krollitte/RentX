47c6c1a415b723776ab86b88fc0f0f5de5ba7095
RafaelSantos <rsantoslfap@gmail.com>
2022-03-01 01:39:02 -0300

    Cadastro de usuario

--- src/screens/SignUp/SignUpSecondStep/index.tsx
+++ src/screens/SignUp/SignUpSecondStep/index.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import {
+  Alert,
+  Keyboard,
   KeyboardAvoidingView,
   TouchableWithoutFeedback,
-  Keyboard,
 } from "react-native";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { useTheme } from "styled-components";
+import { api } from "../../../services/api";
+
 import { BackButton } from "../../../components/BackButton";
 import { Bullet } from "../../../components/Bullet";
 import { PasswordInput } from "../../../components/PasswordInput";
@@ -20,7 +23,6 @@ import {
   Form,
   FormTitle,
 } from "./styles";
-import { Alert } from "react-native";
 
 interface Params {
   user: {
@@ -43,7 +45,7 @@ export function SignUpSecondStep() {
     navigation.goBack();
   }
 
-  function handleRegister() {
+  async function handleRegister() {
     if (!password || !passwordConfirm) {
       return Alert.alert("Informe a senha e a confirmação dela");
     }
@@ -51,12 +53,27 @@ export function SignUpSecondStep() {
     if (password !== passwordConfirm) {
       return Alert.alert("As senhas não são iguais");
     }
-    navigation.navigate("Confirmation", {
-      nextScreenRoute: "SignIn",
-      title: "Conta Criada",
-      message: `Agora é só fazer login \ne aproveitar`,
-    });
+
+    await api
+      .post("/users", {
+        name: user.name,
+        email: user.email,
+        driver_license: user.driverLicense,
+        password,
+      })
+      .then(() => {
+        navigation.navigate("Confirmation", {
+          nextScreenRoute: "SignIn",
+          title: "Conta Criada",
+          message: `Agora é só fazer login \ne aproveitar`,
+        });
+      })
+      .catch((error) => {
+        console.log("error", error);
+        Alert.alert("Opa", "Não foi possível cadastrar");
+      });
   }
+
   return (
     <KeyboardAvoidingView behavior={"position"} enabled>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
